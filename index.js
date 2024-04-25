61d51e3f0b0795148ab770f234c2be4d3d91b4c7
Alena <92329824+poletala@users.noreply.github.com>
2024-04-25 12:45:51 +0500

    month

--- index.js
+++ index.js
@@ -3,18 +3,7 @@ const getCurrentTimeDate = () => {
     let currentTimeDate = new Date();
 
     let month = new Array();
-    month[0] = "Jan";
-    month[1] = "Feb";
-    month[2] = "Mar";
-    month[3] = "Apr";
-    month[4] = "May";
-    month[5] = "Jun";
-    month[6] = "Jul";
-    month[7] = "Aug";
-    month[8] = "Sep";
-    month[9] = "Oct";
-    month[10] = "Nov";
-    month[11] = "Dec";
+    month = ["Jan", "Feb", "Mar", "Apr", "May",  "Jun",  "Jul",  "Aug",  "Sep", "Oct", "Nov", "Dec"]
 
 let hours   =  currentTimeDate.getHours();
 let minutes =  currentTimeDate.getMinutes();
