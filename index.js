8b9e3dc02e2981859b26a5e388e22eaaca3ac62a
Alena <92329824+poletala@users.noreply.github.com>
2024-04-25 12:42:12 +0500

    ls

--- index.js
+++ index.js
@@ -29,56 +29,122 @@ let CurrentYear = currentTimeDate.getFullYear();
 let fullDate = `${currentDate} ${currentMonth}, ${CurrentYear}`;
 
 document.querySelector("time").innerHTML = currentTime;
-// document.querySelectorAll("time")[1].innerHTML = fullDate;
+document.querySelectorAll("time")[1].innerHTML = fullDate;
 
 setTimeout(getCurrentTimeDate, 500);
 }
 
 getCurrentTimeDate();
 
+/* кнопки и переменные */
 
-// Кнопка Добавить
-const addButton = document.querySelector('.add-task-btn');
-// 
-let todoTask = document.querySelector('.todo-list');
-let todoTitleMain = document.querySelector('.title-area-main');
-let todoDetailMain = document.querySelector('.detail-area-main');
-let toAddTitle = document.querySelector('.title-area');
-let toAddDetail = document.querySelector('.detail-area');
-let todo = document.querySelector('#todo-bar')
+let addButton = document.querySelector('.add-task-btn')
+let mainPageButton = document.querySelector('.back-btn')
 
-/*сохраняем данные, внесенные пользователем, в локал сторэдж*/
-function save() {
-    localStorage.setItem('todo-title', toAddTitle.value);
-    localStorage.setItem('todo-detail', toAddDetail.value);
+let inputTitle = document.querySelector('.title-area')
+let inputDetail = document.querySelector('.detail-area')
+let ul = document.querySelector('#todo-bar')
+let arrTasks = []
+
+/* Функции */
+
+function listMain() {
+    let storedData = localStorage.getItem('todoList')
+        if (storedData) {
+            let todoListTask = JSON.parse(storedData)
+            for (i=0;i++;i<todoListTask.length) {
+                let taskHTML = `<li class="todo-list">
+                    <div class="todo-title">
+                        <span class="title-area-main">${todoListTask[i].title}</span>
+                        <span class="detail-area-main">${todoListTask[i].detail}</span>
+                    </div>
+                    <div class="todo-buttons">
+                        <button class="todo-edit" onclick="location.href='./edit.html'" type="button"></button>
+                        <button class="todo-delete"></button>
+                        <button class="todo-done"></button>
+                    </div>
+                    </li>`;
+                    ul.insertAdjacentHTML(beforeend,taskHTML)
+              }
+          } else {
+            console.log('Data not found in local storage')
+          }
+          console.log(storedData)
+          console.log( JSON.parse(storedData))
+}
+    
+function clearFields() {
+    inputTitle.value = '';
+    inputDetail.value = '';
 }
 
-/* записываем данные туду из локал сторэдж */
-function init() {
-    let fromStorageTitle = localStorage.getItem('todo-title');
-    let fromStorageDetail = localStorage.getItem('todo-detail');
-    // if (fromStorageDetail && fromStorageTitle) {
-        let todoHTML = `<li class="todo-list">
-        <div class="todo-title">
-            <span class="title-area-main">${fromStorageTitle}</span>
-            <span class="detail-area-main">${fromStorageDetail}</span>
-        </div>
-        <div class="todo-buttons">
-            <button class="todo-edit" onclick="location.href='./edit.html'" type="button"></button>
-            <button class="todo-delete"></button>
-            <button class="todo-done"></button>
-        </div>
-    </li>`;
-        todo.insertAdjacentHTML('beforeend', todoHTML) 
-    // }
+function addTaskToLocalStorage() {
+    let todoListItem = {
+        title: inputTitle.value,
+        detail: inputDetail.value
+    }
+    console.log(todoListItem)
+    arrTasks.push(todoListItem)
+
+    localStorage.setItem('todoList', JSON.stringify(arrTasks));
+    console.log(arrTasks)
 }
 
 
-// При нажатии на кнопку ADD добавляетяс заголовок и детали дела
 
 addButton.addEventListener('click', () => {
-    save()
-    init()
+    let storedData = JSON.parse(localStorage.getItem('todoList'));
+    if (!!storedData && storedData.length !== 0) {
+    addTaskToLocalStorage()
+    clearFields()
+    }
+    else {
+        let arrTasks = [{
+            title: inputTitle.value,
+            detail: inputDetail.value}];
+        clearFields()
+        localStorage.setItem('todoList', JSON.stringify(arrTasks));
+    }
+    return
 })
 
-let toDoListArray = [];
+
+listMain()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let todoList = [
+//     {title:1},
+//     {title:2}
+// ];
+// function getTodoList(alltasks) {
+//     const liElements = alltasks.map((elem) => {
+//         const li = document.createElement('li');
+//         li.classList.add('.todo-list');
+//         li.append(elem.title);
+//         console.log('li', li)
+//         return li
+//     })
+//     return ul.append(...liElements)
+// }
+// getTodoList(todoList)
