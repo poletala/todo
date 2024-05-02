30ba7a73244033114ae4ed87efaf7fe28d7a5fe0
Alena <92329824+poletala@users.noreply.github.com>
2024-05-02 10:08:37 +0500

    mapped

--- scripts/editTask.js
+++ scripts/editTask.js
@@ -14,33 +14,56 @@ function editTask(elem) {
     location.replace("./pages/edit.html");   
 }
 
-/* ф-ция удаления задания из local storage по id */
-
-function deleteTask() {
-    let idToDelete = JSON.parse(localStorage.getItem('idEdit'));
-    for (var i in dataStored) {
-        if (idToDelete === dataStored[i].id) {
-                dataStored.splice(i, 1);
-                localStorage.setItem('todoList', JSON.stringify(dataStored));
-            }
-        }
-}
-
-/* ф-ция добавления редактированного задания в local storage */
-
-function editedTaskLocalStorage() {
-
-    let editedItem = {
-        title: editedTitle.value,
-        detail: editedDetail.value,
-        id: Math.random().toString(16).slice(2)
-    }
-    dataStored.push(editedItem)
-    localStorage.setItem('todoList', JSON.stringify(dataStored));
-}
+/*                               Первый вариант с использованием map */
 
 function updateTask() {
-    deleteTask();
-    editedTaskLocalStorage() 
+    let idTaskToEdit = JSON.parse(localStorage.getItem('idEdit'));
+    const newdataStored = dataStored.map(obj => {
+        if (obj.id === idTaskToEdit) {
+          return {...obj, title: editedTitle.value, detail: editedDetail.value};
+        }
+        return obj;
+      });
+    // console.log(newArr)
+    localStorage.setItem('todoList', JSON.stringify(newdataStored));
     location.replace("../index.html");
 }
+
+
+/*                                Второй вариант с удалением элемента и добавлением нового через push */
+
+/* ф-ция удаления задания из local storage по id */
+
+// function deleteTask() {
+//     let idToDelete = JSON.parse(localStorage.getItem('idEdit'));
+//     for (var i in dataStored) {
+//         if (idToDelete === dataStored[i].id) {
+//                 dataStored.splice(i, 1);
+//                 localStorage.setItem('todoList', JSON.stringify(dataStored));
+//             }
+//         }
+// }
+
+// /* ф-ция добавления редактированного задания в local storage */
+
+// function editedTaskLocalStorage() {
+
+//     let editedItem = {
+//         title: editedTitle.value,
+//         detail: editedDetail.value,
+//         id: Math.random().toString(16).slice(2)
+//     }
+//     dataStored.push(editedItem)
+//     localStorage.setItem('todoList', JSON.stringify(dataStored));
+// }
+
+// function updateTask() {
+//     deleteTask();
+//     editedTaskLocalStorage() 
+//     location.replace("../index.html");
+// }
+
+
+
+
+  
